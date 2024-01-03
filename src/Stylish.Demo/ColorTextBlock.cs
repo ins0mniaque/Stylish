0c27ad026e35ee79045e0d0517f82f6fe8af98e4
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2024-01-02 22:11:45 -0500

    Add ColorTextBlock prototype

---
+++ src/Stylish.Demo/ColorTextBlock.cs
@@ -0,0 +1,70 @@
+﻿using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Stylish.Demo;
+
+public class ColorTextBlock : VisualHost
+{
+    public string Text
+    {
+        get => (string) GetValue ( TextProperty );
+        set => SetValue ( TextProperty, value );
+    }
+
+    public static readonly DependencyProperty TextProperty = RegisterVisualProperty < ColorTextBlock, string? > ( nameof ( Text ), null );
+
+    protected override Visual? GenerateChild ( ref Size availableSize )
+    {
+        var dpi = VisualTreeHelper.GetDpi ( this );
+        var textFormat = new DirectWrite.TextFormat ( TextElement.GetFontFamily ( this ).Source, (float) TextElement.GetFontSize ( this ) );
+        var textLayout = new DirectWrite.TextLayout ( Text, textFormat, (float) dpi.PixelsPerDip );
+
+        // TODO: Reuse surface
+        using var surface = new DirectWrite.Surface ( (int) textLayout.Metrics.WidthIncludingTrailingWhitespace, (int) textLayout.Metrics.Height, (float) dpi.PixelsPerInchX, (float) dpi.PixelsPerInchY );
+
+        var brush = new DirectWrite.Brushes.SolidColorBrush ( surface, new DirectWrite.Brushes.Color ( 1, 0, 0, 0 ) );
+
+        surface.Draw ( textLayout, brush );
+
+        surface.Render ( );
+
+        var bitmap = Imaging.CreateBitmapSourceFromHBitmap ( surface.HBitmap,
+                                                             (nint) 0,
+                                                             new ( 0, 0, surface.PixelWidth, surface.PixelHeight ),
+                                                             BitmapSizeOptions.FromEmptyOptions ( ) );
+
+        var drawingVisual  = new DrawingVisual ( );
+        var drawingContext = drawingVisual.RenderOpen ( );
+
+        drawingContext.DrawImage ( bitmap, new Rect ( 0, 0, surface.Width, surface.Height ));
+
+        drawingContext.Close ( );
+
+        availableSize = new Size ( surface.Width, surface.Height );
+
+        return drawingVisual;
+    }
+
+    protected override void OnDpiChanged ( DpiScale oldDpi, DpiScale newDpi )
+    {
+        base.OnDpiChanged ( oldDpi, newDpi );
+
+        InvalidateMeasure ( );
+        InvalidateChild   ( );
+    }
+
+    protected override void OnPropertyChanged ( DependencyPropertyChangedEventArgs e )
+    {
+        base.OnPropertyChanged ( e );
+
+        // TODO: Override metadata
+        if ( e.Property == FlowDirectionProperty )
+        {
+            InvalidateMeasure ( );
+            InvalidateChild   ( );
+        }
+    }
+}
--- src/Stylish.Demo/MainWindow.xaml.cs
+++ src/Stylish.Demo/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
-using System.Globalization;
 using System.Windows.Documents;
-using System.Windows.Interop;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 
 namespace Stylish.Demo;
 
@@ -11,67 +7,24 @@ public partial class MainWindow
     public MainWindow ( )
     {
         InitializeComponent ( );
-
-        var dpi2 = DirectWrite.DirectX.GetSystemPixelDensity ( );
-        var dpi3 = DirectWrite.DirectX.GetWindowPixelDensity ( new WindowInteropHelper ( this ).EnsureHandle ( ) );
-        var dpi = VisualTreeHelper.GetDpi ( this );
-
-        var textFormat = new DirectWrite.TextFormat ( FontFamily.Source, 48 );
-        var textLayout = new DirectWrite.TextLayout ( "🐋📈🎏🔨💃🙆🐔💌💠😾", textFormat, (float) dpi.PixelsPerDip );
-        var textLayout2 = new DirectWrite.TextLayout ( "ABCDEFGH", textFormat, (float) dpi.PixelsPerDip );
-
-        using var surface = new DirectWrite.Surface ( (int) textLayout.Metrics.WidthIncludingTrailingWhitespace, (int) textLayout.Metrics.Height, (float) dpi.PixelsPerInchX, (float) dpi.PixelsPerInchY );
-
-        var brush = new DirectWrite.Brushes.SolidColorBrush ( surface, new DirectWrite.Brushes.Color ( 1, 0, 0, 0 ) );
-
-        surface.Draw ( textLayout, brush );
-
-        surface.Render ( );
-
-        Image.Source = Imaging.CreateBitmapSourceFromHBitmap(surface.HBitmap, (nint) 0, new System.Windows.Int32Rect ( 0, 0, surface.PixelWidth, surface.PixelHeight ),
-                                                           BitmapSizeOptions.FromWidthAndHeight( surface.Width, surface.Height ) );
-
-        // surface.Resize ( (int) textLayout2.Metrics.WidthIncludingTrailingWhitespace, (int) textLayout2.Metrics.Height, (float) dpi.PixelsPerInchX, (float) dpi.PixelsPerInchY );
-        surface.Clear ( );
-
-        surface.Draw ( textLayout2, brush );
-
-        surface.Render ( );
-
-        var pw2 = surface.PixelDensity.ToPixelWidth  ( (int) textLayout2.Metrics.WidthIncludingTrailingWhitespace );
-        var ph2 = surface.PixelDensity.ToPixelHeight ( (int) textLayout2.Metrics.Height );
-
-        Image2.Source = Imaging.CreateBitmapSourceFromHBitmap(surface.HBitmap, (nint) 0, new System.Windows.Int32Rect ( 0, 0, pw2, ph2 ),
-                                                              BitmapSizeOptions.FromWidthAndHeight( (int) textLayout2.Metrics.WidthIncludingTrailingWhitespace, (int) textLayout2.Metrics.Height ) );
-
-        var test = textLayout2.LineMetrics;
-        var testB = textLayout.ClusterMetrics;
-
-        test?.ToString ( );
-    }
-
-    protected override void OnRender ( DrawingContext drawingContext )
-    {
-        base.OnRender ( drawingContext );
     }
 
     private void RichTextBox_TextChanged ( object sender, System.Windows.Controls.TextChangedEventArgs e )
     {
-        foreach ( var x in e.Changes )
+        foreach ( var change in e.Changes )
         {
-            if ( x.AddedLength > 0 )
+            if ( change.AddedLength > 0 )
             {
-                var range = new TextRange ( RichTextBox.Document.ContentStart.GetPositionAtOffset ( x.Offset ),
-                    RichTextBox.Document.ContentStart.GetPositionAtOffset ( x.Offset + x.AddedLength ) );
+                var range = new TextRange ( RichTextBox.Document.ContentStart.GetPositionAtOffset ( change.Offset ),
+                                            RichTextBox.Document.ContentStart.GetPositionAtOffset ( change.Offset + change.AddedLength ) );
 
                 if ( range.Text.Contains ( 'a', StringComparison.Ordinal ) )
                 {
-                    var lol = RichTextBox.Selection.Start.GetOffsetToPosition ( RichTextBox.Selection.Start );
+                    var caret = RichTextBox.CaretPosition;
 
                     range.Text = range.Text.Replace ( 'a', 'b' );
-                    // RichTextBox.Document.ContentStart.Paragraph.Inlines.Add ( );
 
-                    RichTextBox.Selection.Select ( RichTextBox.Selection.Start.GetPositionAtOffset ( lol + 1 ), RichTextBox.Selection.End.GetPositionAtOffset ( lol + 1 ) );
+                    RichTextBox.CaretPosition = caret.GetNextInsertionPosition ( LogicalDirection.Forward );
                 }
             }
         }
